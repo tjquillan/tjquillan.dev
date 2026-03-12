f7258e1ff6cc6a334f154af0c9fd51b7acea68a1
tjquillan <tjquillan@gmail.com>
2026-03-12 00:56:14 -0500

    feat: Add Prettier for code formatting and integrate it with ESLint.

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,12 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import eslint from "@eslint/js";
 import eslintPluginAstro from "eslint-plugin-astro";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig(
   globalIgnores(["dist/", ".astro/", "public/pagefind/"]),
   eslint.configs.recommended,
   tseslint.configs.strict,
   eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
 );
