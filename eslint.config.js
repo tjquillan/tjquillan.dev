0da5c8d535f7f0c53fc8559b6e685bf845fcf71e
tjquillan <tjquillan@gmail.com>
2026-03-12 00:34:06 -0500

    feat: Set up ESLint with Astro and TypeScript support.

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslint from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  globalIgnores(["dist/", ".astro/", "public/pagefind/"]),
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  eslintPluginAstro.configs.recommended,
+);
