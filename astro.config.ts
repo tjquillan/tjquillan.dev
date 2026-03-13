d98f01a258c1f11c4890801e432a19462331b991
tjquillan <tjquillan@gmail.com>
2026-03-12 22:55:59 -0500

    feat: adopt astro-paper style.
    Introduce a base layout component, configure a custom font, and establish foundational global and typography styles.

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -8,7 +8,16 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://tjquillan.dev",
   integrations: [sitemap()],
-
+  fonts: [
+    {
+      name: "Google Sans Code",
+      cssVariable: "--font-google-sans-code",
+      provider: fontProviders.google(),
+      fallbacks: ["monospace"],
+      weights: [300, 400, 500, 600, 700],
+      styles: ["normal", "italic"],
+    },
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
