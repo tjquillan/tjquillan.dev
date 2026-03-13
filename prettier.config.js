8e1512dd3e943cb00f520244cb31d5ed75aa5f30
tjquillan <tjquillan@gmail.com>
2026-03-13 17:09:04 -0500

    feat: Add Prettier override to use Astro parser for `.astro` files.

--- prettier.config.js
+++ prettier.config.js
@@ -5,4 +5,12 @@
 export default {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   tailwindStylesheet: "./src/styles/global.css",
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
