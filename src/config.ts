90f0b43bfe5f8a8fc2bafd820441c7cfb3e01eee
tjquillan <tjquillan@gmail.com>
2026-03-16 17:10:47 -0700

    feat: change website title.
    May change it back or workshop this but I like it better for now

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ export const CONFIG = {
   website: "https://tjquillan.dev",
   author: "Thomas Quillan",
   description: "A personal website for Thomas Quillan",
-  title: "Thomas Quillan",
+  title: "<tjquillan/>",
   postPerPage: 10,
 };
 
