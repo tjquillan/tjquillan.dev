2865cf674e535eed9ca63be3cf2bf2962da9fdb3
tjquillan <tjquillan@gmail.com>
2026-04-16 22:43:32 -0700

    feat: Integrate astro-expressive-code

--- astro.config.ts
+++ astro.config.ts
@@ -1,29 +1,58 @@
+import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
+
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import {
-  transformerNotationDiff,
-  transformerNotationHighlight,
-  transformerNotationWordHighlight,
-} from "@shikijs/transformers";
+import copyIcon from "@tabler/icons/outline/copy.svg?raw";
 import tailwindcss from "@tailwindcss/vite";
+import expressiveCode, { createInlineSvgUrl } from "astro-expressive-code";
 import pagefind from "astro-pagefind";
 import { defineConfig, fontProviders } from "astro/config";
 
 import { CONFIG } from "./src/config";
 
+const expressiveCodeOptions: AstroExpressiveCodeOptions = {
+  themes: ["one-dark-pro"],
+  styleOverrides: {
+    borderColor: "var(--border)",
+    codeFontFamily: "var(--font-google-sans-code)",
+    codeBackground: "var(--muted)",
+    codeForeground: "var(--foreground)",
+    focusBorder: "var(--accent)",
+    gutterHighlightForeground: "var(--foreground)",
+    uiFontFamily: "var(--font-google-sans-code)",
+    frames: {
+      copyIcon: createInlineSvgUrl(copyIcon),
+      editorActiveTabBackground: "var(--muted)",
+      editorActiveTabForeground: "var(--foreground)",
+      editorActiveTabBorderColor: "var(--border)",
+      editorActiveTabIndicatorBottomColor: "var(--muted)",
+      editorTabBarBackground: "var(--background)",
+      editorTabBarBorderColor: "var(--border)",
+      editorTabBarBorderBottomColor: "var(--border)",
+      editorBackground: "var(--muted)",
+      terminalTitlebarBackground: "var(--background)",
+      terminalTitlebarForeground: "var(--foreground)",
+      terminalTitlebarBorderBottomColor: "var(--border)",
+      terminalTitlebarDotsForeground: "var(--accent)",
+      terminalBackground: "var(--muted)",
+      inlineButtonForeground: "var(--accent)",
+      inlineButtonBackground: "var(--accent)",
+      inlineButtonBorder: "var(--accent)",
+      tooltipSuccessBackground: "var(--accent)",
+      tooltipSuccessForeground: "var(--background)",
+    },
+  },
+};
+
 // https://astro.build/config
 export default defineConfig({
   site: CONFIG.website,
-  integrations: [sitemap(), mdx(), pagefind()],
-  markdown: {
-    shikiConfig: {
-      transformers: [
-        transformerNotationDiff({ matchAlgorithm: "v3" }),
-        transformerNotationHighlight(),
-        transformerNotationWordHighlight(),
-      ],
-    },
-  },
+  integrations: [
+    sitemap(),
+    expressiveCode(expressiveCodeOptions),
+    mdx(),
+    pagefind(),
+  ],
   fonts: [
     {
       name: "Google Sans Code",
